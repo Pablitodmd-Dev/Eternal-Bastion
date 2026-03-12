e4ee86e1727ae0f0e338649ade7a01db242bdbdf
santiago22 <tasiomartin33@gmail.com>
2026-03-12 12:46:09 +0100

    mana generation

---
+++ Scripts/tutorial.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var mana: float = 50.0
+var max_mana: float = 100.0
+var mana_regeneration_rate: float = 1.5
+@onready var mana_label: Label = $CanvasLayer/ManaLabel
+
+func _process(delta):
+	if mana < max_mana:
+		mana += mana_regeneration_rate * delta
+		mana = clamp(mana, 0, max_mana)
+		mana_label.text = "Mana: " + str(int(mana))
+
+func can_cast_spell(cost: float) -> bool:
+	return mana >= cost
+
+func consume_mana(amount: float):
+	mana -= amount
