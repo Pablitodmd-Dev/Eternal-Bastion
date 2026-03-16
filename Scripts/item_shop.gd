3aeff94bc2725cf0b2813fedcdc2e017e657b462
santiago22 <tasiomartin33@gmail.com>
2026-03-16 13:57:23 +0100

    spikes

--- Scripts/item_shop.gd
+++ Scripts/item_shop.gd
@@ -13,10 +13,19 @@ func load_catalog() -> void:
 		catalog = JSON.parse_string(file.get_as_text())
 		print("Catalog loaded: ", catalog.keys())
 
-func _on_texture_button_3_pressed(id: String = "lightning") -> void: 
+func _on_texture_button_6_pressed(id: String = "lightning") -> void: 
 	if catalog.has(id):
 		item_selected.emit(catalog[id])
 		
+		var focus_owner = get_viewport().gui_get_focus_owner()
+		if focus_owner:
+			focus_owner.release_focus()
+
+
+func _on_texture_button_5_pressed(id: String = "spike") -> void:
+	if catalog.has(id):
+		item_selected.emit(catalog[id])
+	
 		var focus_owner = get_viewport().gui_get_focus_owner()
 		if focus_owner:
 			focus_owner.release_focus()
