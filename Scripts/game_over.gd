410170c84990ab2639e1fb5865de4c9c605f880e
santiago22 <tasiomartin33@gmail.com>
2026-03-20 13:29:30 +0100

    retry button

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -3,3 +3,8 @@ extends CanvasLayer
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+
+
+func _on_texture_button_2_pressed() -> void:
+	pass # Replace with function body.
