ef0a8feed310597cfdc24c088b4d87026279012a
santiago22 <tasiomartin33@gmail.com>
2026-03-23 12:30:20 +0100

    win screen

--- Scripts/Level1Timer.gd
+++ Scripts/Level1Timer.gd
@@ -1,14 +1,35 @@
 extends CanvasLayer
 
-@onready var level_timer = get_node("../LevelTimer")
 @onready var time_label = $MarginContainer/PanelContainer/HBoxContainer/TimeLabel
+@onready var level_timer = get_node("../LevelTimer")
+
+var time_is_up: bool = false
 
 func _process(_delta: float) -> void:
-	if level_timer and not level_timer.is_stopped():
-		update_timer_display(level_timer.time_left)
-
-func update_timer_display(time_remaining: float) -> void:
-	var minutes = int(time_remaining) / 60
-	var seconds = int(time_remaining) % 60
+	if not time_is_up:
+		update_ui()
 	
+	if time_is_up:
+		check_final_victory()
+
+func update_ui() -> void:
+	var time_left = level_timer.time_left
+	var minutes = int(time_left) / 60
+	var seconds = int(time_left) % 60
 	time_label.text = "%02d:%02d" % [minutes, seconds]
+
+func check_final_victory() -> void:
+	var enemies_remaining = get_tree().get_nodes_in_group("enemies")
+	
+	if enemies_remaining.size() == 0:
+		show_win_screen()
+
+func show_win_screen() -> void:
+	get_tree().paused = false 
+	
+	get_tree().change_scene_to_file("res://scenes/you_won.tscn")
+
+func _on_level_timer_timeout() -> void:
+	time_is_up = true
+	time_label.text = "00:00"
+	print("¡Tiempo agotado! Limpia los enemigos restantes...")
